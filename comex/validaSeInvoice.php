939bb8174d4517add4c52be0f0fdda5964c41a2c
lucasbairros <lucas.s.bairros@hotmail.com>
2025-07-15 22:24:38 -0300

    Ajuste validação

--- comex/validaSeInvoice.php
+++ comex/validaSeInvoice.php
@@ -9,7 +9,7 @@ $path = '/var/www/html/twn/api/storage/.00000001/.invoice';
 $treinamento = file_get_contents($path);
 
 $parser = new Parser();
-$conteudo_arquivo = $parser->parseFile('/var/www/html/twn/comex/015558-24/PACKINGLIST.pdf')->getText();
+$conteudo_arquivo = $parser->parseFile('/var/www/html/twn/comex/015558-24/INVOICE_MEX26235.pdf')->getText();
 $prompt = $treinamento." ". $conteudo_arquivo;
 
 //print_r($prompt);die();
